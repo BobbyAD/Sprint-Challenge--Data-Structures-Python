3fc69da5be75f617c4fa8be3129866f68be72cbb
Robert Driskell <driskell.ba@gmail.com>
2019-09-20 11:10:42 -0500

    finished stretch

--- names/names.py
+++ names/names.py
@@ -12,14 +12,35 @@ f.close()
 
 duplicates = []
 
-names_cache = {}
+#O(n) solution
+# names_cache = {}
 
-for n in names_1:
-    names_cache[n] = 1
+# for n in names_1:
+#     names_cache[n] = 1
 
+# for n in names_2:
+#     if n in names_cache:
+#         duplicates.append(n)
+
+#Limited memory solution, only store in arrays
+#Sort first array, binary search each name in second array
+#Slower than my first solution, but still pretty dang fast
+#O( n log n )
+names_1.sort()
 for n in names_2:
-    if n in names_cache:
-        duplicates.append(n)
+    first = 0
+    last = len(names_2) - 1
+    found = False
+    while (first <= last and not found):
+        mid = (first + last)//2
+        if names_1[mid] == n:
+            duplicates.append(n)
+            found = True
+        else:
+            if n < names_1[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
